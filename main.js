ea5cc68d2c30080cdcf12d5fc731265b1939cf01
dakoo096 <noelia_cabral96@hotmail.com>
2023-07-09 18:27:53 -0300

    se agregan animaciones en index y comprar-tickets,se corrigen estilos,se corrige script  para mostrar correctamente el total,.se agregan funciones

--- main.js
+++ main.js
@@ -22,6 +22,24 @@ function topFunction() {
 /*----------------
 COMPRAR TICKETS
 ----------------*/
+
+//si se hace click en un div de arriba se selecciona la categoria automaticamente
+function seleccionarCategoria(categoria) {
+  document.getElementById('categoria').value = categoria;
+}
+
+//validar datos del formulario
+function validarFormulario() {
+  const form = document.querySelector('.needs-validation');
+  if (form.checkValidity()) {
+    form.classList.add('was-validated');
+    mostrarResumen();
+    document.getElementById("resumenContainer").style.display = "block";
+  } else {
+    form.classList.add('was-validated');
+  }
+}
+
 //se establecen las variables
 function mostrarResumen() {
   var nombre = document.getElementById("nombre").value;
@@ -42,7 +60,8 @@ function mostrarResumen() {
   }
 
   var total = cantidad * precio * (1 - descuento);
-  document.getElementById("total").textContent = total.toFixed(2);
+  document.getElementById("total").textContent = total.toFixed( 2);
+
 
   //se crea objeto resumen con los datos ingresados
   var resumenHTML = `
@@ -54,18 +73,47 @@ function mostrarResumen() {
     <p>Total a Pagar: $${total.toFixed(2)}</p>
   `;
 
-  //se muestran los datos
   document.getElementById("resumen").innerHTML = resumenHTML;
-  document.getElementById("resumenContainer").style.display = "block";
 }
+  //se muestran los datos
+  document.getElementById("cantidad").addEventListener("input", mostrarResumen);
+  document.getElementById("categoria").addEventListener("input", mostrarResumen);
 
+
+function enviarDatos() {
+  // Obtener los datos del resumen
+  var nombre = document.getElementById("nombre").value;
+  var apellido = document.getElementById("apellido").value;
+  var correo = document.getElementById("correo").value;
+  var cantidad = parseInt(document.getElementById("cantidad").value);
+  var categoria = document.getElementById("categoria").value;
+  var total = parseFloat(document.getElementById("total").textContent);
+
+  // Realizar la solicitud AJAX o cualquier otra acción para enviar los datos al servidor
+  // Aquí se muestra un mensaje de ejemplo en la consola
+  console.log("Datos enviados:");
+  console.log("Nombre:", nombre);
+  console.log("Apellido:", apellido);
+  console.log("Correo:", correo);
+  console.log("Cantidad:", cantidad);
+  console.log("Categoría:", categoria);
+  console.log("Total:", total);
+
+}
 //se crea funcion borrar datos para restablecer todo a 0
 function borrarDatos() {
+  // Restablecer el display del resumenContainer a "none"
+  document.getElementById("resumenContainer").style.display = "none";
+
+  // Restablecer los valores del formulario a sus valores iniciales
   document.getElementById("nombre").value = "";
   document.getElementById("apellido").value = "";
   document.getElementById("correo").value = "";
-  document.getElementById("cantidad").value = "1";
-  document.getElementById("categoria").selectedIndex = 0;
+  document.getElementById("cantidad").value = 0;
+  document.getElementById("categoria").value = "Estudiante";
   document.getElementById("total").textContent = "";
-  document.getElementById("resumenContainer").style.display = "none";
+
+  // Restablecer la validación del formulario
+  const form = document.querySelector('.needs-validation');
+  form.classList.remove('was-validated');
 }
